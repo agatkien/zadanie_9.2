927a5e4778d91826d36c722ffbda1f8a13211531
Agata Snoch <agatasnoch@Agatas-MacBook-Air.local>
2017-08-08 19:24:56 +0400

    niby poprawione:)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
-var allNames = femaleNames.concat.maleNames;
+var allNames = femaleNames.concat.(maleNames);
 
  console.log(allNames);
 
-var newName = 'Marian'
+var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1){
     
     allNames.push('Marian');
-    console.log(allNames + ' Marian');
+    console.log(allNames);
 }
